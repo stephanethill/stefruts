e4f2419de8073ce210444724e66dd61f66831a23
Stéphane Thill <stephane.thill@hotmail.fr>
2024-06-02 23:57:30 +0200

    Now it's beautiful

--- stefruts/app/page.tsx
+++ stefruts/app/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-screen bg-primary-400 text-secondary-800 select-none">
-      <div className="w-full flex flex-col justify-start items-center p-4 gap-4">
+    <main className="min-h-screen w-screen bg-secondary-50 text-secondary-800 select-none">
+      <div className="w-full flex flex-col justify-start items-start p-4 gap-4">
         <div className="w-full flex flex-col justify-center items-center">
           <Image src="/stefruts.png" alt="Stefruts" width={175} height={175} />
           <p className="text-2xl font-bold text-secondary-800">
@@ -16,8 +16,25 @@ export default function Home() {
             fruits
           </p>
         </div>
+        <div>
+          <p className="text-2xl font-semibold">Comment régler ?</p>
+          <p>Payer moi en liquide ou via PayLib (06 65 96 88 09)</p>
+        </div>
+        <div>
+          <p className="text-2xl font-semibold">
+            J'ai l'impression que c'est pas ouf ce site, non ?
+          </p>
+          <p>
+            Clairement le système est moyennement au point, ce n'est pas très
+            convénient. Cependant ça montrera que la demande de fruits est bien
+            réelle
+          </p>
+        </div>
         <div className="w-full">
-          <FruitsStock />
+          <p className="text-2xl font-semibold">Stock en cours</p>
+          <div className="w-full">
+            <FruitsStock />
+          </div>
         </div>
       </div>
     </main>
