855e65fa9223f342611b7480e28f29c3af2b57b5
Stéphane Thill <stephane.thill@hotmail.fr>
2024-06-03 21:56:33 +0200

    Somes fruits on the side

--- stefruts/app/page.tsx
+++ stefruts/app/page.tsx
@@ -4,30 +4,32 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-screen bg-secondary-50 text-secondary-800 select-none">
-      <div className="w-full flex flex-col justify-start items-start p-4 gap-4">
+    <main className="relative min-h-screen w-screen flex flex-col justify-center items-center text-secondary-800 select-none">
+      <div className="flex-grow w-full max-w-[850px] bg-white flex flex-col justify-start items-start p-4 gap-4">
         <div className="w-full flex flex-col justify-center items-center">
           <Image src="/stefruts.png" alt="Stefruts" width={175} height={175} />
           <p className="text-2xl font-bold text-secondary-800">
             Pour ceux qui veulent des fruits
           </p>
           <p className="text-2xl font-semibold italic text-secondary-800 opacity-50">
-            À cause de ceux qui <span className="text-red-700">volent</span> des
-            fruits
+            À cause de ceux qui <span className="text-primary-500">volent</span>{" "}
+            des fruits
           </p>
         </div>
         <div>
-          <p className="text-2xl font-semibold">Comment régler ?</p>
-          <p>Payer moi en liquide ou via PayLib (06 65 96 88 09)</p>
+          <p className="text-2xl font-semibold">Est-ce que c'est gratuit ?</p>
+          <p>
+            Prenez ce que vous consommez et déposez les fruits que vous
+            souhaitez faire partager
+          </p>
         </div>
         <div>
           <p className="text-2xl font-semibold">
-            J'ai l'impression que c'est pas ouf ce site, non ?
+            Ce site est incroyable, comment ça se fait ?
           </p>
           <p>
-            Clairement le système est moyennement au point, ce n'est pas très
-            convénient. Cependant ça montrera que la demande de fruits est bien
-            réelle
+            Des années de R&D, de design et de storytelling pour arriver à
+            l'aboutissement de tout un travail complexe et complet.
           </p>
         </div>
         <div className="w-full">
@@ -37,6 +39,10 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div
+        className="absolute inset-0 bg-repeat -z-10"
+        style={{ backgroundImage: "url('/fruits.webp')" }}
+      ></div>
     </main>
   );
 }
